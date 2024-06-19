c7bd45d3b45cb06c04fadf209bf66aa29fb0535c
Rohit <pandeyrohit232@gmail.com>
2024-06-19 13:25:45 +0530

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,7 +14,7 @@ const[cityerror,setCityErr]=useState(false);
 const[metric,setMetric]=useState("celsius");
 const[favlist,setFavlist]=useState(false);
 const[loaded,setdataLoaded]=useState(false);
-
+const apiKey=process.env.WEATHER_API_KEY
 const[favouriteloc,setFavouriteLoc]=useState([]);
 var arr=[];
 useEffect(()=>{
@@ -25,7 +25,15 @@ useEffect(()=>{
     }
     getData();
     getfavlocationlist();
+    
 },[favlist])
+const storeRecent=(value)=>{
+     const items=localStorage.getItem("city");
+     var dataValue=[];
+     dataValue.push(items);
+     dataValue.push(value.toUpperCase());
+         localStorage.setItem("city",dataValue);
+}
 
 const valueintitial={
    
@@ -42,6 +50,7 @@ const FavCityWeather=async(item)=>{
                lat:item.latitude,
                lon:item.longitude,
                appid:'8d629db10495c47d8908c307532c333e',
+               units:"metric"
                }))
                
                .then(response=>response.json())
@@ -75,17 +84,12 @@ const getfavlocationlist=async()=>{
                       })
                     //   console.log(favweatherData);
                       
-                      
                 }
                 else{
                     console.log('Loading Favourite City');
-                   
-                   
                 }
-             
         
         }
-       
     
      await getlocdata();
     }
@@ -132,6 +136,7 @@ const handleFavourite=()=>{
 
 const getCityData=()=>{
    const location=inputlocation;
+
    const getCityFetch=async(e)=>{
       await fetch("http://api.openweathermap.org/geo/1.0/direct?"+new URLSearchParams({
         q:e,
@@ -149,11 +154,12 @@ const getCityData=()=>{
         window.alert("Empty Search");
     }
     else{
+        storeRecent(location);
         getCityFetch(location);
       
         if(cityList.length<=0 && cityerror==false)
             {
-               console.log(cityList);
+            //    console.log(cityList);
                setcityList([]);
                 setCityErr(true);
             }
@@ -183,12 +189,17 @@ const SetCoordinate=(e)=>{
     
 
     const getweatherData=async(e)=>{
-       
+        
+        const v1=cityList[e].lon;
+        const v2=cityList[e].lat;
+        // console.log(v1+" "+v2);
         await fetch("https://api.openweathermap.org/data/2.5/forecast?"+new URLSearchParams({
                     
-            lat:cityList[e].lon,
-            lon:cityList[e].lat,
+            lat:v2,
+            lon:v1,
             appid:'8d629db10495c47d8908c307532c333e',
+            units:"metric",
+            
             }))
             
             .then(response=>response.json())
@@ -203,6 +214,7 @@ const SetCoordinate=(e)=>{
 
     }
     getweatherData(e);
+    
    
     
 
@@ -210,17 +222,25 @@ const SetCoordinate=(e)=>{
 
 const extractdata=()=>{
     var temp=[];
-    Object.entries(favweatherData).forEach(([keys, value]) => {
-     Object.entries(value).forEach(([index,ind])=>{
-  //   console.log(ind.main.temp);
-          var obj={city:keys,...value};
-          temp.push(obj);
-              })
-      
-  })
-  console.log(temp);
-  arr=temp;
-  return true;
+    const getDataFav=async()=>{
+        Object.entries(favweatherData).forEach(([keys, value]) => {
+            Object.entries(value).forEach(([index,element])=>{
+
+                 var obj={city:keys,...element};
+                 temp.push(obj);
+                 
+                     })
+         })
+        
+    }
+   getDataFav();
+   
+if(temp.length==(favouriteloc.length*40))
+    {
+        arr=temp;
+        // console.log(arr);
+        return true;
+    }
 }
 
 
@@ -233,6 +253,23 @@ const extractdata=()=>{
         <input type='text' placeholder='Enter Location Name'  name="text-name" onChange={(e)=>{setinputLocation(e.target.value);}}></input>
         <button onClick={()=>{getCityData();
         }}>Find</button><br></br>
+        {
+            (inputlocation)?
+            
+                <div className='grp-list'>
+                    <h4>Recent Search</h4>
+                    
+                    <div className='list'>
+                    <span>{localStorage.getItem("city")}</span>
+                    </div>
+                   
+                </div>
+                
+               
+            
+            :<></>
+        }
+      
 
         {
             (cityList.length>0)?<div className='grp1-list'>
@@ -258,62 +295,69 @@ const extractdata=()=>{
          {/* *My City Open */}
 
          <div style={{marginTop:"5%",fontSize:"8px"}}>
-        <button style={{backgroundColor:"skyblue",border:"white",color:"black",padding:"1%",
+        <button type="radio" style={{border:"white",color:"white",padding:"1%",
         fontWeight:"bold"}} onClick={()=>{setFavlist(!favlist);
         if(favlist===true){}}}>
         {(favlist)?<h4>Close Saved City</h4>:<h4>Open Saved City</h4>}</button><br></br>
     
-        <a href="#favlist"><button style={{padding:"2%",margin:"2%",color:"black",backgroundColor:"skyblue",}} >Go to Saved Weather</button></a>
-        <a href="#weathlist"> <button style={{padding:"2%",color:"black",margin:"2%",backgroundColor:"skyblue",}}>Go to Current Weather</button></a>
+        {(favlist==true  && weatherData.length>0)?<a href="#favlist"><button style={{padding:"2%",margin:"2%",color:"black",backgroundColor:"skyblue",}} >Go to Saved Weather</button></a>:<></>}
+        {(weatherData.length>0)?<a href="#weathlist"> <button style={{padding:"2%",color:"black",margin:"2%",backgroundColor:"skyblue",}}>Go to Current Weather</button></a>:<></>}
     
         </div>
 
         </div>
-{/* Display fetched Weather Data */}
+{/* Displaying the Data */}
      <div className='details'>
          {
             (favlist==false && weatherData.length<=0)?<div className='bg-msg'>Hey! What are  you waiting For?<br></br>Check Your City Weather<br></br><br></br>
             <h1>NOW...</h1></div>:<></>
          }
-        <div>
 
-    {
-        
-        (favlist===true&& extractdata())?
-        
-        <div id='favlist' style={{backgroundColor:"grey",margin:"2%"}}>
-            <div style={{backgroundColor:"black",color:"white",padding:"2%",margin:"2%"}}>
-        <input type="radio" id="celsius" name="celsius" value="celsius" checked={metric==="celsius"}onChange={()=>setMetric("celsius")}/><label htmlFor="celsius">Celsius</label>   
-        <input type="radio" id="fahren" name="fahren" value="fahren"  checked={metric==="fahren"} onChange={()=>setMetric("fahren")}/><label htmlFor="fahren">Fahrenheit</label>
-        <h3>Favourite Cities</h3>
-            </div>
-              {  
-             
-         arr.map((ind,index)=>{
+
+         {/* Display the data of Stored favourite City List */}
+       
+
+            {
+                
+                (favlist===true && extractdata()==true)?
+                
+                <div id='favlist' style={{backgroundColor:"grey",margin:"2%",marginBottom:"15%"}}>
+                    <div style={{backgroundColor:"black",color:"white",padding:"2%",margin:"2%"}}>
+                <input type="radio" id="celsius" name="celsius" value="celsius" checked={metric==="celsius"}onChange={()=>setMetric("celsius")}/><label htmlFor="celsius">Celsius</label>   
+                <input type="radio" id="fahren" name="fahren" value="fahren"  checked={metric==="fahren"} onChange={()=>setMetric("fahren")}/><label htmlFor="fahren">Fahrenheit</label>
+                <h1 style={{fontStyle:"italic"}}>Favourite Cities</h1>
+                <div style={{backgroundColor:"grey",display:"flex",flexWrap:'wrap',padding:"2%",textAlign:"center"}}>
+                {
+                    favouriteloc.map((item)=>{
+                        return <h4 style={{margin:"2%"}}>{item.cityName}</h4>
+                    })
+                }
+                </div>
+                    </div>
+                    { 
+                
+                        arr.map((element,index)=>{
+                            // console.log(ind.city);
+                            // console.log(ind.dt_txt);
+                                    return(   <div  className='favforecast-list' key={index}>
+                                        <Favourite metric={metric} city={element.city} time={(element.dt_txt)} icon={(element.weather[0].icon)} description={(element.weather[0].description)} temp={(element.main.temp).toPrecision(4)}  mintemp={(element.main.temp_min).toPrecision(4)} maxtemp={(element.main.temp_max).toPrecision(4)} 
+                                        feels_like={(element.main.feels_like).toPrecision(4)} humidity={(element.main.humidity)} wind={(element.wind.speed*2.237).toPrecision(4)}/>
+                                        
+                                        </div>
+                                        );
+                            })
+                        
+                    
+                
+                    }
+                    </div>
             
-             console.log(arr.size);
-            //  console.log(ind[index]);
-             if(ind[index]!==undefined)
-                 {
-                     return(   <div className='favforecast-list' key={ind[index].dt_txt}>
-                         <Favourite metric={metric} city={ind.city} time={(ind[index].dt_txt)} icon={(ind[index].weather[0].icon)} description={(ind[index].weather[0].description)} temp={(ind[index].main.temp/10).toPrecision(4)}  mintemp={(ind[index].main.temp_min/10).toPrecision(4)} maxtemp={(ind[index].main.temp_max/10).toPrecision(4)} 
-                         feels_like={(ind[index].main.feels_like/10).toPrecision(4)} humidity={(ind[index].main.humidity)} wind={(ind[index].wind.speed*2.237).toPrecision(4)}/>
-                         </div>
-                         );
-                 }
-          
-         })
-         
-     }
-   
-           
-        </div>
-      
-        :<></>
+            :<></>
+                
+            }
         
-        }
 
-        </div>
+          
        
        {
         ( cityList.length>0 && weatherData.length>0 && cityerror==true)?
@@ -323,26 +367,26 @@ const extractdata=()=>{
      
         {
          (weatherData.length>0)?
-          
-          <div id='weathlist' style={{backgroundColor:"grey",margin:"2%"}}>
+         
+          <div id='weathlist' style={{backgroundColor:"#c7edff",margin:"2%"}}>
             <div style={{backgroundColor:"black",color:"white",padding:"2%",margin:"2%"}}>
-                <h4>Searched City<br></br></h4>
+                <h3>Searched City</h3>
                     <h2>{currentCity}</h2>
                         <input type="radio" id="celsius" name="celsius" value="celsius" checked={metric==="celsius"}onChange={()=>setMetric("celsius")}/><label htmlFor="celsius">Celsius</label>   
                         <input type="radio" id="fahren" name="fahren" value="fahren"  checked={metric==="fahren"} onChange={()=>setMetric("fahren")}/><label htmlFor="fahren">Fahrenheit</label>
         
             </div>
-            <button onClick={handleFavourite}><h5>Add {currentCity} To Favourite</h5></button><br></br>
+            <button style={{border:'1px solid #09eb94',backgroundColor:'#09eb94',color:'black'}}onClick={handleFavourite}><h5>Add To Favourite</h5></button><br></br>
       
         {weatherData.map((item)=>{
-          
+           console.log(weatherData);
             return(
             
             <div className='forecast-list' key={item.dt_txt}>
                 
-                <Forecast  metric={metric} city={(currentCity).toUpperCase()} icon={item.weather[0].icon} time={item.dt_txt} description={item.weather[0].description} temp={(item.main.temp/10).toPrecision(4)}
-                mintemp={(item.main.temp_min/10).toPrecision(4)} maxtemp={(item.main.temp_max/10).toPrecision(4)} 
-                feels_like={(item.main.feels_like/10).toPrecision(4)} humidity={(item.main.humidity)} wind={(item.wind.speed*2.237).toPrecision(4)}
+                <Forecast  metric={metric} city={(currentCity).toUpperCase()} icon={item.weather[0].icon} time={item.dt_txt} description={item.weather[0].description} temp={(item.main.temp).toPrecision(4)}
+                mintemp={(item.main.temp_min).toPrecision(4)} maxtemp={(item.main.temp_max).toPrecision(4)} 
+                feels_like={(item.main.feels_like).toPrecision(4)} humidity={(item.main.humidity)} wind={(item.wind.speed*2.237).toPrecision(4)}
                 />
              
                
