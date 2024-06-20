8f883762188e67de0aa3f7b6c3c5ac2abf894550
Rohit <pandeyrohit232@gmail.com>
2024-06-21 00:20:15 +0530

    commit

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -11,20 +11,18 @@ function Forecast(props) {
    
        
   return (
-    <div className='card'>
-     
-        <img src={'https://openweathermap.org/img/wn/' +props.icon+ '@2x.png' } /><br></br>
-        <span style={{fontWeight:'bold'}}>{props.description.toUpperCase()}</span>
-       
-        <h1>{(props.metric=='celsius')?<>{props.temp}°C</>:<>{((9/5)*(props.temp) + 32).toPrecision(4)}°F</>}</h1>
-        
-        <label>Min: </label><span style={{fontWeight:'bold'}}>{(props.metric=='celsius')?<>{props.mintemp}°C</>:<>{((9/5)*(props.mintemp) + 32).toPrecision(4)}°F</>}</span>&nbsp;&nbsp;&nbsp;
-        <label>Max: </label><span style={{fontWeight:'bold'}}>{(props.metric=='celsius')?<>{props.maxtemp}°C</>:<>{((9/5)*(props.maxtemp) + 32).toPrecision(4)}°F</>}</span><br></br><br></br>
-        <label>Feels:</label><span>{(props.metric=='celsius')?<>{props.feels_like}°C</>:<>{((9/5)*(props.feels_like) + 32).toPrecision(4)}°F</>}</span><br></br>
-        <label>Humidity:</label><span>{props.humidity}%</span><br></br>
-        <label>Wind:</label><span>{props.wind} miles/hr</span>
-        <h3>{props.city}</h3>
-        <h4>{newdate}     {formattedtime}</h4>
+    <div className='card1'>
+         <img src={'https://openweathermap.org/img/wn/' +props.icon+ '@2x.png' } /><br></br>
+      <span style={{fontWeight:'bold',fontFamily:"verdana"}}>{props.description.toUpperCase()}</span>
+      
+      <h1>{(props.metric=='celsius')?<>{props.temp}°C</>:<>{((9/5)*(props.temp) + 32).toPrecision(4)}°F</>}</h1>
+      <h4 style={{fontWeight:'bold'}}>{newdate}&nbsp;&nbsp;&nbsp;&nbsp;{formattedtime}</h4>
+      <label style={{fontWeight:'bold'}}>Min: </label><span style={{fontWeight:'bold'}}>{(props.metric=='celsius')?<>{props.mintemp}°C</>:<>{((9/5)*(props.mintemp) + 32).toPrecision(4)}°F</>}</span><br></br><br></br>
+      <label style={{fontWeight:'bold'}}>Max: </label><span style={{fontWeight:'bold'}}>{(props.metric=='celsius')?<>{props.maxtemp}°C</>:<>{((9/5)*(props.maxtemp) + 32).toPrecision(4)}°F</>}</span><br></br><br></br>
+      <label>Feels:</label><span>{(props.metric=='celsius')?<>{props.feels_like}°C</>:<>{((9/5)*(props.feels_like) + 32).toPrecision(4)}°F</>}</span><br></br>
+      <label>Humidity:</label><span>{props.humidity}%</span><br></br>
+      <label>Wind:</label><span>{props.wind} miles/hr</span>
+      <h3>{props.city}</h3>
         
         
     </div>
